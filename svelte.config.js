88317b802f993912724c35662f8f208a64e0755e
Noah Geerts <ngeerts@student.ubc.ca>
2025-12-06 17:32:09 -0800

    Remove base path for Vercel deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const base = '/slick-portfolio-svelte';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -22,9 +20,6 @@ const config = {
 			'@md': './src/lib/md',
 			'@stores': './src/lib/stores',
 			'@utils': './src/lib/utils'
-		},
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? base : ''
 		}
 	}
 };
